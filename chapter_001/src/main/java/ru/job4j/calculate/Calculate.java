f72e22729f13a6cc38a65ddca74955c819942180
spyckjim <spyckjim@gmail.com>
2018-12-21 16:53:49 +0300

    1.3. JavaDoc. Code convention[#95602]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate.
+ * @author spyckjim
+ */
 public class Calculate {
 	
+	/**
+	 * Main.
+	 * @param args - args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author spyckjim (spyckjim@gmail.com)
+* @version 1
+* @since 21.12.2018
+*/
+package ru.job4j.calculate;
