38acf4048c5848357870b02edeaf288ce698ab2c
spyckjim <spyckjim@gmail.com>
2019-02-05 19:36:22 +0300

    "2.3. Запуск программы, запуск тестов, дебаг. [#95559]"

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -5,23 +5,23 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author spyckjim (spyckjim@gmail.com)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author spyckjim (spyckjim@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/
-@Test
-public void whenTakeNameThenThreeEchoPlusName() {
-    String input = "spyckjim";
-    String expect = "Echo, echo, echo : spyckjim"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenThreeEchoPlusName() {
+        String input = "spyckjim";
+        String expect = "Echo, echo, echo : spyckjim";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
