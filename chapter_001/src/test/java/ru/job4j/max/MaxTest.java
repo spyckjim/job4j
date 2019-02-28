85fdf71b9a9a5747575015e0d46eac4d25e5ae03
spyckjim <spyckjim@gmail.com>
2019-02-28 17:58:33 +0300

    "4.2. Максимум из двух чисел[#95567]"

--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -11,4 +11,11 @@ public class MaxTest {
         int result = maxim.max(1, 2);
         assertThat(result, is(2));
     }
+
+    @Test
+    public void whenFirstMoreSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(2, 1);
+        assertThat(result, is(2));
+    }
 }
